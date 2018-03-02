51be1ee2d1a347f12b767b148d5d41a9f9908184
eran <egrimberg@gmail.com>
2018-03-03 00:18:08 +0200

    ......

---
+++ aws7-start_instance.py
@@ -0,0 +1,11 @@
+import boto3
+
+# Region your instances are in, e.g. 'us-east-1'
+#region = 'XX-XXXXX-X'
+
+# instances ID: ex. ['X-XXXXXXXX', 'X-XXXXXXXX']
+instances = ['i-025a9d571f41dcfd2']   #chef111
+#ec2 = boto3.client('ec2', region_name=region)
+ec2 = boto3.client('ec2')
+ec2.start_instances(InstanceIds=instances)
+print('Started your instances: ' + str(instances))
