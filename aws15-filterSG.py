df70357c429e8415d99bbefd50e91c9025cb48fd
eran <egrimberg@gmail.com>
2018-03-03 11:22:48 +0200

    ......

---
+++ aws15-filterSG.py
@@ -0,0 +1,8 @@
+import boto3
+client = boto3.client('ec2')
+response = client.describe_instances()
+print(response)  # take the output and use http://jsonviewer.stack.hu/
+for r in response['Reservations']:
+    for instance in r["Instances"]:
+        sgid = instance["SecurityGroups"][0]["GroupId"]
+        print(sgid) # print all instances with Reservations/SecurityGroups/GroupId
