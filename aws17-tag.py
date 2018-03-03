512c1c032260a5876a3a3e784efe640a7c7b7a5e
eran <egrimberg@gmail.com>
2018-03-03 12:51:03 +0200

    ......

--- aws17-tag.py
+++ aws17-tag.py
@@ -1,18 +1,20 @@
 import boto3
 import sys
-
+# https://gist.github.com/fideloper/a6296b750e8a73d0da1d54b09dd5749e
 ec2 = boto3.client('ec2')
 
 # Grab where backup retention is 14 days so we can reduce it to 7
 #instances = ec2.describe_instances(Filters=[{'Name': 'tag:Retention', 'Values': ['14']}])
 instances = ec2.describe_instances(Filters=[{'Name': 'tag:RI', 'Values': ['yes']}])
+
 ids = []
 
 for reservation in instances['Reservations']:
     for instance in reservation['Instances']:
+        print(instance["InstanceId"])
         ids.append(instance['InstanceId'])
 
-print "Changing tags for %d instances" % len(ids)
+print("Changing tags for %d instances" % len(ids))
 
 ec2.create_tags(
     Resources=ids,
@@ -23,3 +25,4 @@ ec2.create_tags(
         }
     ]
 )
+
