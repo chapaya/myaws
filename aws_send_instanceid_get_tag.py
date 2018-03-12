5847352373b0617401a374fee0eb58c8316ffdeb
eran <egrimberg@gmail.com>
2018-03-12 23:08:12 +0200

    ......

---
+++ aws_send_instanceid_get_tag.py
@@ -0,0 +1,14 @@
+import boto3
+#https://www.slsmk.com/using-python-and-boto3-to-get-instance-tag-information/
+# When given an instance ID as str e.g. 'i-1234567', return the instance 'Name' from the name tag.
+ec2 = boto3.resource('ec2')
+fid='i-027a13c48bbd73fa3'
+ec2instance = ec2.Instance(fid)
+instancename = ''
+for tags in ec2instance.tags:
+    print(tags) # print all tags
+    if tags["Key"] == 'Name':
+        instancename = tags["Value"]
+print(instancename)
+
+
