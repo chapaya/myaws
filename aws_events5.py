303d0304df24a269a3b0e314be687f7c6fbc164b
eran <egrimberg@gmail.com>
2018-03-23 16:15:37 +0300

    ......

--- aws_events5.py
+++ aws_events5.py
@@ -1,16 +1,37 @@
 import boto3
-#https://www.slsmk.com/using-python-and-boto3-to-get-instance-tag-information/
-# When given an instance ID as str e.g. 'i-1234567', return the instance 'Name' from the name tag.
-ec2 = boto3.resource('ec2')
+ec2resource = boto3.resource('ec2')
+ec2client = boto3.client('ec2')
 #fid='i-027a13c48bbd73fa3'
+#This function get instance id and rerutn name
 def get_name(fid):
-    #ec2instance = ec2.Instance(fid)
+    ec2instance = ec2resource.Instance(fid)
     instancename = ''
-    print(ec2instance)
+    #print(ec2instance)
     for tags in ec2instance.tags:
         #print(tags) # print all tags
         if tags["Key"] == 'Name':
             instancename = tags["Value"]
-            print(instancename)
+            #print(instancename)
+    return(instancename)
 
-get_name(fid='i-027a13c48bbd73fa3')
+#get_name(fid='i-027a13c48bbd73fa3')
+response = ec2client.describe_instance_status()
+#print(response)
+
+# Check events :
+for r in response['InstanceStatuses']:
+     #print(r['InstanceId'])
+     try:
+         value = r['Events']
+     except KeyError:
+         # Key is not present
+         #print(r['InstanceId'])
+         #get_name(fid=r['InstanceId'])
+         print("No event for " + r['InstanceId'] + " server_name=" + get_name(fid=r['InstanceId']))
+     else:
+         print("There is event for " + r['InstanceId'] + " server_name=" + get_name(fid=r['InstanceId']) + "Please Check!")
+
+'''
+No event for i-0fcc21bbfaea6a592 server_name=spree2
+No event for i-0e77545e5a65a2182 server_name=Percona
+'''
