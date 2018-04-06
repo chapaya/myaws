6f86ef60697d2339598369d340ed50160da020fb
eran <egrimberg@gmail.com>
2018-04-07 00:31:13 +0300

    ......

--- aws_vol.py
+++ aws_vol.py
@@ -1,5 +1,7 @@
 import boto3
 ec2 = boto3.resource('ec2', region_name='us-east-1')
+ec2cl = boto3.client('ec2', region_name='us-east-1')
+
 volumes = ec2.volumes.all()
 vol = 'vol-0dd8de038a3c9f8c3'
 
@@ -13,3 +15,18 @@ volumes = ec2.volumes.filter(Filters=[{'Name': 'status', 'Values': ['available']
 for v in volumes:
     #print(v.id) # all volumes
     print("This disk is avalible " + v.id)
+
+print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+response = ec2cl.describe_volume_status(
+    VolumeIds=[
+        'vol-0efca32aee2ab7574',
+    ],
+)
+print(response)
+
+print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+response = ec2cl.describe_volumes()
+print(response)
+for row in response['Volumes']:
+    #print(row[VolumeId])
+    print(row['VolumeId'] , row['Size'] , row['VolumeType'] , row['State'])
