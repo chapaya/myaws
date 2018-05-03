d9f60dc26d7a20fdac25e3f2e4174b934b224910
Eran G <egrimberg@gmail.com>
2018-05-03 15:43:43 +0300

    Update aws_check_uptime_server.py

--- aws_check_uptime_server.py
+++ aws_check_uptime_server.py
@@ -1,7 +1,10 @@
-#check if server up more than 1 day
 import boto3
 import datetime
 from dateutil import parser
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 ec2client = boto3.client('ec2')
 ec2resource = boto3.resource('ec2')
 
@@ -14,29 +17,49 @@ def get_name(fid):
             instancename = tags["Value"]
     return(instancename)
 
+body = []
+send_mail= 0
 response = ec2client.describe_instances()
 
 for reservation in response['Reservations']:
     for instance in reservation['Instances']:
-        strchk = "myserver"
+        strchk = "myserver_name"
         servername=str(get_name(fid=instance["InstanceId"]))
         if strchk in servername:
            #print("Instance_id=" + instance["InstanceId"] + " Name=" + get_name(fid=instance["InstanceId"]) + " Lunchtime:" + str(instance["LaunchTime"]))
            current_time = (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-           #print(current_time)
            luntime = str(instance["LaunchTime"])
-           #print(luntime)
            luntime1 = parser.parse(luntime)
            launchtime_naive = luntime1.replace(tzinfo=None)
            then = datetime.datetime.utcnow() + datetime.timedelta(days = -1)
            if launchtime_naive < then:
-               print("NOT OK - " + get_name(fid=instance["InstanceId"]) + " -" + " Lunchtime: " + str(instance["LaunchTime"]) + " run more than a day !")
+               print("Server " + get_name(fid=instance["InstanceId"]) + " -" + " Lunchtime: " + str(instance["LaunchTime"]) + " run more than a day !")
+               str1 = ("Server - " + get_name(fid=instance["InstanceId"]) + " -" + " Lunchtime: " + str(instance["LaunchTime"]) + " run more than a day !")
+               body.append(str1)
                print("EC2 lunch time: " + str(luntime1))
                print("One day ago time: " + str(then))
+               send_mail = 1
            else:
                print("OK - " + get_name(fid=instance["InstanceId"]) + " -" + " Lunchtime: " + str(instance["LaunchTime"]) + " run less than a day")
-               print("EC2 lunch time: " + str(luntime1))
-               print("One day ago time: " + str(then))
            print("------------------------------------------------------------------------------------------------------------------------")
         else:
            x=1
+
+body1 = ('\n'.join(body))
+print(body1)
+
+# Mail setup
+if send_mail == 1:
+   server = smtplib.SMTP('xxxxx')
+   fromaddr = "xxxxxm"
+   toaddr = "xxxx"
+   msg = MIMEMultipart('alternative')
+   msg['Subject'] = "server EC2 - up more than a day"
+   msg['From'] = fromaddr
+   msg['To'] = toaddr
+   msg.attach(MIMEText(body1, 'plain'))
+   text = msg.as_string()
+   server.sendmail(fromaddr, toaddr, text)
+   server.quit()
+else:
+   print("All OK ! No mail !")
